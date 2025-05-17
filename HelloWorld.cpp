d8785d83b00f19d11ccce6013c050d9e589ddbb4
theGitCoder-DW <jitesh.girisha@gmail.com>
2025-05-17 11:05:25 +0200

    My changes

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+int max(int n1, int n2);
 
 int main()
 {
@@ -10,6 +11,23 @@ int main()
     res=a+b;
 
     cout<< "result " << res << endl;
-
+    int n1=5;
+    int n2=6;
+    int ret;
+    ret = max(n1,n2);
+    cout<< "Max Val is :" << ret <<endl;
     return 0;
+}
+
+int max(int n1, int n2)
+{
+    int res;
+
+    if(n1<n2)
+        res=n1;
+
+    else
+        res=n2;
+
+    return res;
 }
