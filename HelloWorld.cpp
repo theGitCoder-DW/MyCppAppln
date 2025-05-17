b46ce016dfbe4d7d48296e48bbc817e9548c6470
theGitCoder-DW <jitesh.girisha@gmail.com>
2025-05-17 10:40:34 +0200

    Initialtry of cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "Hello World" << endl;
+
+    int a,b,res;
+
+    res=a+b;
+
+    cout<< "result " << res << endl;
+
+    return 0;
+}
